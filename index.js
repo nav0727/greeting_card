f87e67619374b0a74ad82567d1e05ce0626568e1
nav0727 <dosaninaveenreddy@gmail.com>
2022-09-18 12:11:02 +0000

    firstcommit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="heading"> Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+
+      <h1 className="para">Kiran V</h1>
+      <p className="para">
+        Vishnu Institute of Computer Education And Technology,Bhimavaram{" "}
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
